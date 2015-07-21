9efa32d79cf63367612e07a32f43035356614a58
Chris McCormick <chris@mccormick.cx>
2015-07-21 19:23:36 +0800

    CLI usage changes.

--- blender-livecode.sh
+++ blender-livecode.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "$1" == "" ]
 then
-	echo "Watches HYLANGFILE for changes and reloads in Blender"
 	echo "Usage: $0 HYLANGFILE"
+	echo "Watches HYLANGFILE for changes in Blender and reloads on save, frame change:"
 	echo "e.g. $0 entry.hy"
 else
 	blender --python entry-py-watcher.py $1
